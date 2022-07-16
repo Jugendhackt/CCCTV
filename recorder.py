1e1dad5caa809eabf98f6eee9766bf8580c69572
Idle02 <idle.client@gmail.com>
2022-07-16 19:49:15 +0100

    changed file name

--- recorder.py
+++ recorder.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == '__main__':
     try:
-        PiCamera().start_recording(f'/tmp/picture{sys.argv[1]}.h264')
+        PiCamera().start_recording(f'/tmp/recording{sys.argv[1]}.h264')
         sleep(5)
         piCamera().stop_recording()
     except:
