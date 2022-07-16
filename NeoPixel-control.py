fe2514f2daedd22d09dfd32ee8aefb0d2062ac58
Tams-Tams <Tams.Mathe@gmail.com>
2022-07-16 15:00:27 +0200

    Update NeoPixel-control.py

--- NeoPixel-control.py
+++ NeoPixel-control.py
@@ -17,18 +17,26 @@ class LED:
             self.colors = [(20, 100, 20), (100, 50, 20)]
 
     def start(self):
-        while True:
-            for ind in range(len(pixels)):
-                if ind % 2 == 1:
-                    pixels[ind] = self.colors[0]
-                else:
-                    pixels[ind] = (0, 0, 0)
-            pixels.show()
-            time.sleep(0.5)
-            for ind in range(len(pixels)):
-                if ind % 2 == 0:
-                    pixels[ind] = self.colors[1]
-                else:
-                    pixels[ind] = (0, 0, 0)
-            pixels.show()
-            time.sleep(0.5)
+        for ind in range(len(pixels)):
+            if ind % 2 == 1:
+                pixels[ind] = self.colors[0]
+            else:
+                pixels[ind] = (0, 0, 0)
+        pixels.show()
+        time.sleep(0.5)
+        for ind in range(len(pixels)):
+            if ind % 2 == 0:
+                pixels[ind] = self.colors[1]
+            else:
+                pixels[ind] = (0, 0, 0)
+        pixels.show()
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    led = LED()
+    count = 0
+    while True:
+        led.start()
+        led.change_state(count % 5)
+        count += 1
