8f6acce89b36747b4ed66f814c9d01e7b4ca31b6
Marco <74458051+IT-BsB@users.noreply.github.com>
2022-07-16 20:04:14 +0200

    Update and rename sendung.py to email.py

--- email.py
+++ email.py
@@ -1,19 +1,19 @@
 from mailjet_rest import Client
 import os
-api_key = '58271d695ba38436aaea0a5fa6b2ba1b'
-api_secret = 'f84f3646abdaf5b3866abb5f396cd10f'
+api_key = '******************************'
+api_secret = '******************************'
 mailjet = Client(auth=(api_key, api_secret), version='v3.1')
 data = {
   'Messages': [
     {
       "From": {
-        "Email": "fayiko4911@runfons.com",
-        "Name": "Marco"
+        "Email": "your@email.com",
+        "Name": "name"
       },
       "To": [
         {
-          "Email": "fayiko4911@runfons.com",
-          "Name": "Marco"
+          "Email": "your@email.com",
+          "Name": "name"
         }
       ],
       "Subject": "Greetings from Mailjet.",
