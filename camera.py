43ef1694e0ffc7ee87d1da43ea06183596f3fdc1
Marco <74458051+IT-BsB@users.noreply.github.com>
2022-07-17 10:32:59 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -6,7 +6,6 @@ camera = PiCamera()
 
 if __name__ == '__main__':
     try:
-        
         camera.capture(f'/tmp/pct/picture{sys.argv[1]}.jpg')
         camera.stop_preview()
         camera.close()
